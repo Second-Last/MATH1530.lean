a5c285411e6444b576e8a1a3302891862b661129
Gavin Zhao <gavinz@brown.edu>
2024-10-10 22:22:02 -0400

    CH1_2: complete
    Signed-off-by: Gavin Zhao <gavinz@brown.edu>

--- MATH1530/Basic.lean
+++ MATH1530/Basic.lean
@@ -1,1 +1,35 @@
-def hello := "world"
+class Group (carrier : Type) where
+  op : carrier → carrier → carrier
+  e : carrier
+  e_right : ∀ x : carrier, op x e = x
+  e_left : ∀ x : carrier, op e x = x
+  op_assoc : ∀ x y z, op x (op y z) = op (op x y) z
+  inv : carrier → carrier
+  op_inv_left : ∀ x, op (inv x) x = e
+  op_inv_right: ∀ x, op x (inv x) = e 
+
+postfix:max "⁻¹" => Group.inv
+infixl:73 " • " => Group.op
+
+open Group 
+
+theorem inv_eq_of_mul_eq_one_left {G : Type} [Group G] 
+  (a b : G) : a • b = e → a⁻¹ = b :=  λ a_op_b_is_e =>
+    let a_inv_right : a • a⁻¹ = e := op_inv_right a
+    let eq : a⁻¹ • (a • a⁻¹) = a⁻¹ • (a • b) := congrArg _ <| Eq.trans a_inv_right (Eq.symm a_op_b_is_e)
+
+    let lhs : a⁻¹ • (a • a⁻¹) = a⁻¹ := (calc a⁻¹ • (a • a⁻¹)
+      _ = (a⁻¹ • a) • a⁻¹ := op_assoc _ _ _
+      _ = e • a⁻¹ := by rw [op_inv_left a]
+      _ = a⁻¹ := by rw [e_left a⁻¹])
+
+    let rhs : a⁻¹ • (a • b) = b := (calc a⁻¹ • (a • b)
+      _ = (a⁻¹ • a) • b := op_assoc _ _ _
+      _ = e • b := by rw [op_inv_left a]
+      _ = b := by rw [e_left b])
+
+    by rw [←lhs, ←rhs]; assumption
+
+class Subgroup (carrier : Type) (parent : Type) [Group parent] where
+  is_subset : carrier → parent
+  carrier_is_group : Group carrier
--- MATH1530/CH1_2.lean
+++ MATH1530/CH1_2.lean
@@ -1,10 +1,11 @@
+/- import MATH1530.Basic -/
 import Mathlib
 
 namespace MATH1530
 
 namespace CH1_2
 
-theorem th4 {α : Type} [Group α] (a b : α) : (a * b)⁻¹ = b⁻¹ * a⁻¹ :=
+theorem th4 {G : Type} [Group G] (a b : G) : (a • b)⁻¹ = b⁻¹ * a⁻¹ :=
   let rhs : (b⁻¹ * a⁻¹) * (a * b) = 1 := 
     (calc (b⁻¹ * a⁻¹) * (a * b)
       _ = b⁻¹ * a⁻¹ * a * b := by rw [←mul_assoc (b⁻¹ * a⁻¹) a b]
@@ -13,6 +14,10 @@ theorem th4 {α : Type} [Group α] (a b : α) : (a * b)⁻¹ = b⁻¹ * a⁻¹ :
       _ = b⁻¹ * b := by rw [mul_one b⁻¹]
       _ = 1 := inv_mul_cancel b)
   inv_eq_of_mul_eq_one_left rhs
+
+/- theorem th4 {G : Type} [Group G] (a b : G) : (a • b)⁻¹ = b⁻¹ • a⁻¹ :=  -/
+/-   let lhs_op_rhs_is_e : (a • b) • (b⁻¹ • a⁻¹) = e := sorry -/
+/-   inv_eq_of_mul_eq_one_left (a • b) (b⁻¹ • a⁻¹) lhs_op_rhs_is_e -/
 
 end CH1_2
 
