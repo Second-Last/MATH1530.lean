3f8ccfc8ec8c979ae9334469edadcb5da55a85c6
Gavin Zhao <gavinz@brown.edu>
2024-10-10 23:04:00 -0400

    CH3: th2
    Signed-off-by: Gavin Zhao <gavinz@brown.edu>

--- MATH1530/CH3.lean
+++ MATH1530/CH3.lean
@@ -10,11 +10,6 @@ namespace CH3
 
 open Group
 
-/- theorem Subgroup.nonempty_and_forall_mul_inv_mem -/
-/-     {G : Type*} [Group G] (H : Subgroup G) : -/
-/-     (∃ x : G, x ∈ H) ∧ ∀ x ∈ H, ∀ y ∈ H, x * y⁻¹ ∈ H := -/
-/-   sorry -/
-
 theorem th1 {G : Type*} [Group G] (H : Set G)
   : IsSubgroup H ↔ (H.Nonempty ∧ ∀ (a b : G), a ∈ H → b ∈ H → (a * b⁻¹) ∈ H) 
   := Iff.intro fwd_direction bwd_direction
@@ -48,6 +43,37 @@ theorem th1 {G : Type*} [Group G] (H : Set G)
 
         And.intro h_is_nonempty a_b_inv_in_h
     
+
+theorem th2 {G : Type*} [Group G] (H : Set G)
+  : IsSubgroup H ↔ (H.Nonempty ∧ (∀ (a b : G), a ∈ H → b ∈ H → (a * b) ∈ H) ∧ (∀ (a : G), a ∈ H → a⁻¹ ∈ H)) := 
+    let cond_is_same : 
+      (H.Nonempty ∧ ∀ (a b : G), a ∈ H → b ∈ H → (a * b⁻¹) ∈ H) ↔
+      (H.Nonempty ∧ (∀ (a b : G), a ∈ H → b ∈ H → (a * b) ∈ H) ∧ (∀ (a : G), a ∈ H → a⁻¹ ∈ H)) := 
+        let fwd_direction :
+          (H.Nonempty ∧ ∀ (a b : G), a ∈ H → b ∈ H → (a * b⁻¹) ∈ H) →
+          (H.Nonempty ∧ (∀ (a b : G), a ∈ H → b ∈ H → (a * b) ∈ H) ∧ (∀ (a : G), a ∈ H → a⁻¹ ∈ H)) 
+          | ⟨h_is_nonempty, rel⟩ => 
+            let ⟨x, x_in_h⟩ := h_is_nonempty
+            let x_op_invx_in_h : x * x⁻¹ ∈ H := rel x x x_in_h x_in_h
+            let one_in_h : 1 ∈ H := Eq.subst (mul_inv_cancel x) x_op_invx_in_h
+            let ab_in_h (a b : G) (ha : a ∈ H) (hb : b ∈ H) : (a * b) ∈ H := 
+              let b_inv_in_h : b⁻¹ ∈ H := Eq.subst (one_mul b⁻¹) (rel 1 b one_in_h hb)
+              (inv_inv b) ▸ (rel a b⁻¹ ha b_inv_in_h)
+            let a_inv_in_h (a : G) (ha : a ∈ H) : a⁻¹ ∈ H := one_mul a⁻¹ ▸ rel 1 a one_in_h ha
+            And.intro h_is_nonempty (And.intro ab_in_h a_inv_in_h)
+
+        let bwd_direction :
+          (H.Nonempty ∧ (∀ (a b : G), a ∈ H → b ∈ H → (a * b) ∈ H) ∧ (∀ (a : G), a ∈ H → a⁻¹ ∈ H)) →
+          (H.Nonempty ∧ ∀ (a b : G), a ∈ H → b ∈ H → (a * b⁻¹) ∈ H)
+          | ⟨h_is_nonempty, ⟨rel1, rel2⟩⟩ =>
+            let a_b_inv_in_h (a b : G) (ha : a ∈ H) (hb : b ∈ H) : (a * b⁻¹) ∈ H := 
+              rel1 a b⁻¹ ha (rel2 b hb)
+            And.intro h_is_nonempty a_b_inv_in_h
+
+        Iff.intro fwd_direction bwd_direction
+
+    (propext cond_is_same) ▸ (th1 H)
+
 
 end CH3
 
